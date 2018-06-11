c95c4663c4f710cc764a8231c6268fe8d13bad17
zproo <630816477@qq.com>
2018-06-11 21:20:57 +0800

    feat: Use protostuff to realize encoder & decoder module in messaging layer.

--- rpc-registry-zookeeper/src/main/java/indi/zproo/zrpc/registry/zookeeper/Constant.java
+++ rpc-registry-zookeeper/src/main/java/indi/zproo/zrpc/registry/zookeeper/Constant.java
@@ -5,7 +5,16 @@ package indi.zproo.zrpc.registry.zookeeper;
  * @create: 2018-06-07 16:08
  **/
 public interface Constant {
+
+	// 接口中变量默认都是 public static final的。
+	/**
+	 * zookeepper 中会话过期时间
+	 */
 	int ZK_SESSION_TIMEOUT = 5000;
+
+	/**
+	 * zkclient 需要的连接超时时间
+	 */
 	int ZK_CONNECTION_TIMEOUT = 1000;
 
 	String ZK_REGISTRY_PATH = "/registry";
